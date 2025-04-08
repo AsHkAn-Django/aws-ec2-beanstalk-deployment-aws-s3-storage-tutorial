e19b04b124f84f08eddc47f5ff38ab5a316213b0
Ashkan <ashkan_ahrari@yahoo.com>
2025-04-08 14:32:46 +0300

    last changes

--- photoAlbumViewer/settings.py
+++ photoAlbumViewer/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'photoAlbumViewer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'NAME': 'djangophoto',
+        'USER': 'admin',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
