4408be3cc737b1291e895ed255148ea5b9c98f99
Ashkan Ahrari <46989230+AsHkAn-Django@users.noreply.github.com>
2025-04-09 18:16:57 +0300

    Update settings.py

--- photoAlbumViewer/settings.py
+++ photoAlbumViewer/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'my_db',
-        'USER': 'postgres',
+        'USER': 'admin',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '5432',
