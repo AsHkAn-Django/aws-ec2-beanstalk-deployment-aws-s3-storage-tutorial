17f28c4e589b2870b3387e84f0c5dc41ce6c5f6a
Ashkan Ahrari <46989230+AsHkAn-Django@users.noreply.github.com>
2025-04-09 20:12:35 +0300

    Update urls.py

--- photoAlbumViewer/urls.py
+++ photoAlbumViewer/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myApp.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+if settings.DEBUG or True: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
