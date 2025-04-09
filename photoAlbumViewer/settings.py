a71f44e839502bb995415335832c7c40e17ebf60
Ashkan <ashkan_ahrari@yahoo.com>
2025-04-09 12:24:43 +0300

    changed the db back to sqllite again

--- photoAlbumViewer/settings.py
+++ photoAlbumViewer/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'photoAlbumViewer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql.psycopg2',
-        'NAME': 'djangophoto',
-        'USER': 'admin',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
